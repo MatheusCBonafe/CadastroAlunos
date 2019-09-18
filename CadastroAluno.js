059fea78e3cd3dd6e3f7192f31451ecbb2366b86
MatheusCBonafe <warpmatheus@gmail.com>
2019-09-17 21:56:33 -0300

    Adição de função instável

--- CadastroAluno.js
+++ CadastroAluno.js
@@ -1,5 +1,73 @@
+var arrayAlunos = [];
+
+class Aluno {
+	constructor(nome, ra, sexo, idade, tel, end, email) {
+		this.nome = nome;
+		this.ra = ra;
+		this.sexo = sexo;
+		this.idade = idade;
+		this.tel = tel;
+		this.end = end;
+		this.email = email;
+	}
+
+}
+function registerStudent() {
+
+	var sexo;
+
+	var radios = myForm["sexo"];
+
+	var nome = document.getElementById("firstName").innerHTML;
+
+	var ra = document.getElementById("ra").innerHTML;
+
+	var idade = document.getElementById("age").innerHTML;
+
+	var tel = document.getElementById("tel").innerHTML;
+
+	var end = document.getElementById("end").innerHTML;
+
+	var email = document.getElementById("email").innerHTML;
+
+	if (radios[0].checked)
+		sexo = "Masculino"
+		else if (radios[1].checked)
+				sexo = "Feminino";
+		else
+			sexo = "Não definido";
+
+
+
+	var student = new Aluno (nome, ra, sexo, idade, tel, end, email)
+	arrayAlunos.push(student);
+}
+
+
+
 function showAll() {
-	var myForm = document.forms["formulario"];
+
+	document.getElementById("campos").innerHTML = "Alunos";
+
+	for (var aux = 0; aux < arrayAlunos.length; aux++) {
+		document.getElementById("show_nome").innerHTML = "Nome: " + arrayAlunos[aux].nome;
+
+		document.getElementById("show_ra").innerHTML = 	"RA: " + arrayAlunos[aux].ra;
+
+		document.getElementById("show_sexo").innerHTML = 	"Sexo: " + arrayAlunos[aux].sexo;
+
+		document.getElementById("show_idade").innerHTML = "Idade: " + arrayAlunos[aux].idade;
+
+		document.getElementById("show_tel").innerHTML = "Telefone: " + arrayAlunos[aux].tel;
+
+		document.getElementById("show_end").innerHTML = "Endereço: " + arrayAlunos[aux].end;
+
+		document.getElementById("show_email").innerHTML = "E-Mail: " + arrayAlunos[aux].email;
+
+	}
+
+
+/*	var myForm = document.forms["formulario"];
 
 	document.getElementById("campos").innerHTML = "Alunos";
 
@@ -29,5 +97,5 @@ function showAll() {
 			document.getElementById("show_sexo").innerHTML = "Sexo: Não definido";
 		}
 	}
-	return false;
+	return false;*/
 }
